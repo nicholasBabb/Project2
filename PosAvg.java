570ac1c4ceabf25ca12d05c4d93032275f54f8ca
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-08 12:09:41 -0500

    Implemented readFile, expandArray, indexOfStation Methods.

--- PosAvg.java
+++ PosAvg.java
@@ -9,8 +9,11 @@ import java.io.IOException;
  */
 public class PosAvg {
 	
-	private String[] storedStations;
+	// Stores stations read in through readFile, initialized with 1 "slot"
+	private String[] storedStations = new String[1];
 	
+	//Stores sets of stations whose indexes average out to become the index of stID, initialized to 1 "slot"
+	private String[] averageStations = new String[1];
 	/**
 	 * Constructor for PosAvg
 	 * @param stID
@@ -18,10 +21,42 @@ public class PosAvg {
 	public PosAvg(String stID) {
 		String stationId = stID;
 	}
-	public void readIn (String fileName) throws IOException {
+	// Taken from Nicholas Babb - Project 1. Variable names and method functions were modified
+	// to accommodate the nature of this project, as well as suggestions given during code review.
+	public void readFile (String fileName) throws IOException {
+		// Holds entire line
+		String readLineStorage = "Default";
+		// Holds substring of readLineStorage, station name specifically
+		String readLineSubstring = "Default";
+		int index = 0;
+		// Beginning of Station ID after being trimmed.
+		int beginString = 0;
+		// End of Station ID after being trimmed.
+		int endString = 3;
 		BufferedReader readIn = new BufferedReader(new FileReader(fileName));
-		
-	}
+		// Reading through first 3 lines to ensure it doesn't go into the array
+		readLineStorage = readIn.readLine();
+		readLineStorage = readIn.readLine();
+		readLineStorage = readIn.readLine();
+		// Reading in the rest of the file
+		readLineStorage = readIn.readLine();
+		readLineStorage.trim();
+		for (index = 0; readLineStorage != null; index++) {
+			
+			// Scanning in only the station name with a substring
+			readLineSubstring = readLineStorage.substring(beginString, endString);
+			
+			// Determining if storedTerms can hold next term.
+			if (storedStations.length <= index) {
+				expandArray();
+			}
+			storedStations[index] = readLineSubstring;
+			readLineStorage = readIn.readLine();
+			
+		}
+		readIn.close();
+    }
+	
 	// Taken from Nicholas Babb - Project 1. Variable names and 
 	// increase in array size changed to accommodate names in this project.
 	public void expandArray () {
@@ -30,6 +65,30 @@ public class PosAvg {
 			temp[i] = storedStations[i];
 		}
 		this.storedStations = temp;
+	}
+	
+	public int indexOfStation (String stationId) {
+		int indexOfStation = 0;
+		for (int i = 0; i < storedStations.length; i++) {
+			if (stationId.equalsIgnoreCase(storedStations[i])) {
+				indexOfStation = i;
+				return indexOfStation;
+			}
+		}
+		return -1;
+	}
+	
+	public int stationAveragePositions (int indexOfStation) {
+		// Will cover indexes ahead of indexOfStation
+		int forwardIndex = indexOfStation + 1;
+		// Will cover indexes behind indexOfStation
+		int backwardIndex = indexOfStation - 1;
+		// Will contain the pairing of stations whose indexes average out to become indexOfStation
+		String averageStationPair = "Default";
+		for (int i = 1; storedStations[forwardIndex] == null || storedStations[backwardIndex] == null;) {
+			
+		}
+			
 	}
 	
 }
