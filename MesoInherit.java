479aaf1c0e803320c71ca2eac8ceaf5c15d5a106
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-08 19:02:50 -0500

    Implemented 2 method names

--- MesoInherit.java
+++ MesoInherit.java
@@ -3,7 +3,9 @@
  *  Ascii Ceiling is 79
  *  Ascii Floor is 78
  *	Ascii Average is 79
- *
+ *	char e = 't';
+ *	int whatever = (int) e; 	I GIVE ASCII
+ *	
  *  Letter Avg: O
  * (3-6)
  */
@@ -11,6 +13,15 @@ public class MesoInherit {
 	
 	public MesoInherit(MesoStation mesoStation) {
 		String stationId = mesoStation.getStID();
+	}
+	
+	// Calculate Ascii values. Inherited from MesoAbstract
+	public int[] calAverage() {
+		stationId.
+	}
+	// Calculate average letter value. Inherited from MesoAbstract
+	public char letterAverage() {
+		
 	}
 
 }
