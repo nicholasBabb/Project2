89c3e39c6f03bc4eb4bb3a4350eab2cec0e62b57
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-08 20:31:21 -0500

    Implemented calAverage method

--- MesoInherit.java
+++ MesoInherit.java
@@ -11,13 +11,29 @@
  */
 public class MesoInherit {
 	
+	private String stationId;
+			
 	public MesoInherit(MesoStation mesoStation) {
-		String stationId = mesoStation.getStID();
+		stationId = mesoStation.getStID();
 	}
 	
-	// Calculate Ascii values. Inherited from MesoAbstract
+	// Calculate ASCII values. Inherited from MesoAbstract
 	public int[] calAverage() {
-		stationId.
+		// To hold floor, ceiling, and average. Initialized to 3 slots as that is all that is needed
+		int[] asciiAverages = new int[3];
+		double averageValue = 0;
+		//Splitting stationId into a char array to easily access each letter for conversion to ASCII.
+		char[] charArray = stationId.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			int asciiValue = (int) charArray[i];
+			averageValue += (double) asciiValue;
+		}
+		// Divided by 4 because there are 4 letters in a Station ID.
+		averageValue = (averageValue / 4.0);
+		// Round to nearest value for Average
+		asciiAverages[2] = (int) Math.round(averageValue);
+		asciiAverages[1] = (int) Math.floor(averageValue);
+		asciiAverages[0] = (int) Math.ceil(averageValue);
 	}
 	// Calculate average letter value. Inherited from MesoAbstract
 	public char letterAverage() {
